f0df67b4da97d041f0f81c42b07de37c01f01075
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2026-07-26 11:25:59 -0400

    Add tests (which may be skipped) for cyclic port positioning.

--- run-tests.scm
+++ run-tests.scm
@@ -77,4 +77,44 @@
                   (lambda (p)
                     (get-string-n p 8))))
 
+(test-group "port positioning"
+  ;; Skip next test if cyclic bytevector ports aren't positionable.
+  (call-with-port
+   (open-cyclic-input-bytevector '#vu8(1 2 3))
+   (lambda (p)
+     (unless (and (port-has-port-position? p)
+                  (port-has-set-port-position!? p))
+       (test-skip 1))))
+
+  (test-assert "read on cyclic bytevector port after positioning"
+    (call-with-port
+     (open-cyclic-input-bytevector '#vu8(1 2 3))
+     (lambda (p)
+       (let* ((get-bvec (lambda () (get-bytevector-n p 5)))
+              (_junk (get-bytevector-n p 13))
+              (pos (port-position p))
+              (v (get-bvec)))
+         (set-port-position! p pos)
+         (equal? v (get-bvec))))))
+
+  ;; Skip next test if cyclic string ports aren't positionable.
+  (call-with-port
+   (open-cyclic-input-string "abc")
+   (lambda (p)
+     (unless (and (port-has-port-position? p)
+                  (port-has-set-port-position!? p))
+       (test-skip 1))))
+
+  (test-assert "read on cyclic string port after positioning"
+    (call-with-port
+     (open-cyclic-input-string "abc")
+     (lambda (p)
+       (let* ((get-str (lambda () (get-string-n p 5)))
+              (_junk (get-string-n p 13))
+              (pos (port-position p))
+              (s (get-str)))
+         (set-port-position! p pos)
+         (equal? s (get-str))))))
+  )
+
 (test-end)
